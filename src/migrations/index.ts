4dcf3979b7c4c6db164b59f90687d0cb52f6a1b5
developkosarev <develop.kosarev@gmail.com>
2026-01-31 17:57:09 +0100

    init

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260131_162154 from './20260131_162154';
+
+export const migrations = [
+  {
+    up: migration_20260131_162154.up,
+    down: migration_20260131_162154.down,
+    name: '20260131_162154'
+  },
+];
